e39c888a345196e2d6d50b3cf1822ef623a22b5c
mrichardsJBOIT <14255080+mrichardsJBOIT@users.noreply.github.com>
2021-06-13 15:16:52 +1000

    Finish JS gallery

--- js/gallery.js
+++ js/gallery.js
@@ -1,7 +1,12 @@
+// Activate the image gallery
+
 function activeGallery() {
 	let thumbnails = document.querySelectorAll("#gallery-thumbs > div > img");
 	let mainImage  = document.querySelector("#gallery-photo img");
 	const currentClass = "current";
+	// image info to be updated
+
+
   thumbnails.forEach(function(thumbnail) {
     thumbnail.addEventListener("click", function(){
 			//Set clicked image as the main image
@@ -13,7 +18,14 @@ function activeGallery() {
 			//change which image is current
 			document.querySelector(`.${currentClass}`).classList.remove(currentClass);
 			thumbnail.parentNode.classList.add(currentClass);
+
+			let galleryInfo = document.querySelector("#gallery-info");
+			let title = galleryInfo.querySelector(".title");
+			let description = galleryInfo.querySelector(".description");
+
+			title.innerHTML 			= thumbnail.dataset.title;
+			description.innerHTML = thumbnail.dataset.description;
 		});
   });
-	console.log(thumbnails.innerText);
+
 }
