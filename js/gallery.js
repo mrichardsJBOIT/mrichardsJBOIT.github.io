00a14ae9d00857c19a5881f77d7190a1dfc9577e
mrichardsJBOIT <14255080+mrichardsJBOIT@users.noreply.github.com>
2021-06-13 15:04:27 +1000

    section 11.3 setting current image

--- js/gallery.js
+++ js/gallery.js
@@ -1,12 +1,18 @@
 function activeGallery() {
 	let thumbnails = document.querySelectorAll("#gallery-thumbs > div > img");
 	let mainImage  = document.querySelector("#gallery-photo img");
-
+	const currentClass = "current";
   thumbnails.forEach(function(thumbnail) {
     thumbnail.addEventListener("click", function(){
 			//Set clicked image as the main image
 			let newSrc = thumbnail.dataset.largeVersion;
+			let newAlt = thumbnail.getAttribute("alt");
 			mainImage.setAttribute("src", newSrc);
+			mainImage.setAttribute("alt", newAlt);
+
+			//change which image is current
+			document.querySelector(`.${currentClass}`).classList.remove(currentClass);
+			thumbnail.parentNode.classList.add(currentClass);
 		});
   });
 	console.log(thumbnails.innerText);
