80993e404d07805a8394fc0786a828d00ac2f452
mrichardsJBOIT <14255080+mrichardsJBOIT@users.noreply.github.com>
2021-06-13 14:42:18 +1000

    Learn enough JavaScrip - section 11.2 changing iaage

--- js/gallery.js
+++ js/gallery.js
@@ -1,3 +1,13 @@
 function activeGallery() {
-	alert("Hello from the gallery file!");
+	let thumbnails = document.querySelectorAll("#gallery-thumbs > div > img");
+	let mainImage  = document.querySelector("#gallery-photo img");
+
+  thumbnails.forEach(function(thumbnail) {
+    thumbnail.addEventListener("click", function(){
+			//Set clicked image as the main image
+			let newSrc = thumbnail.dataset.largeVersion;
+			mainImage.setAttribute("src", newSrc);
+		});
+  });
+	console.log(thumbnails.innerText);
 }
